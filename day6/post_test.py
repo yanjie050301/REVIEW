90a7fe7d7b4d7753965827bfc84bff55d0d5ceab
yanjie050301 <1353037583@qq.com>
2021-02-21 21:32:34 +0800

    补充携带cookies发送请求的4种方法

--- day6/post_test.py
+++ day6/post_test.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-
+"""
 # 1.请求地址
 urlstr = "https://wanandroid.com/user/login"
 # 2.发送请求
@@ -19,7 +19,7 @@ username = res_result["data"]["username"]
 #响应断言
 if errorCode == 0 and username == datas.get("username"):
     print("登陆成功")
-
+"""
 
 ############json传参练习
 # post请求，传参方式是以json格式传参
@@ -54,49 +54,89 @@ print(r2.text)
 """
 
 #练习查快递的接口
-
-
-
-
-
-
-
-
-
-# 携带cookie发送请求
+#####################打开快递网：http://www.kuaidi.com/，搜索某个单号，判断它的状态是不是已签收
 """
+url = "http://www.kuaidi.com/index-ajaxselectcourierinfo-773058962040428-shentong-UUCAO1613872252544.html"
+herders = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
+          }
+s = requests.session()
+re = s.post(url = url)
+res = re.json()
+print(res)
+data = res["data"][0]["context"]
+# print(data)
+if "包裹已签收"in data:
+    print("包裹已签收")
+else:
+    print("包裹未签收")
+
+"""
+# 携带cookie发送请求
+
 ###########方法一##############通过r.cookies手动获取上一请求返回的cookie来设置下次请求的cookie
+"""
 # 1.请求地址
 urlstr = "https://wanandroid.com/user/login"
 # 2.发送请求
 datas = {"username":"yanjie000","password":"123123"}
 r = requests.post(url=urlstr,data=datas)
-print("********",r.cookies)
-print("**",r.headers)
-# 获取cookie值传给下一个请求
-cookie = r.cookies
-print("555",cookie)
-r2 = requests.get("https://wanandroid.com/1g/todo/list/0",cookies = cookie)
-print(r2.status_code)
+#手动获取上一个请求的cookies
+coo = r.cookies
+url1 = "https://www.wanandroid.com/lg/todo/list/0"
+#带入下一个请求中，进行提交
+r1 = requests.get(url = url1,cookies =coo)
+print(r1.text,r1.status_code)
 """
 ###########方法二##############通过requests.session自动设置cookie，来完成访问
-
-
-
-
-
+"""
+# 1.请求地址
+urlstr = "https://wanandroid.com/user/login"
+# 2.发送请求
+datas = {"username":"yanjie000","password":"123123"}
+#初始化session对象
+s = requests.session()
+#通过session对象发送请求，服务器设置在本地的cookie会自动保存到本地
+r = s.post(url=urlstr,data=datas)
+#带入下一个请求中，进行提交
+url1 = "https://www.wanandroid.com/lg/todo/list/0"
+r1 = s.get(url = url1)
+print(r1.status_code)
+"""
 ###########方法三##############通过定制cookie，单独设置cookie来访问目标网址
-
-
-
-
-
-
+"""
+# 1.请求地址
+urlstr = "https://wanandroid.com/user/login"
+# 2.发送请求
+datas = {"username":"yanjie000","password":"123123"}
+r = requests.post(url = urlstr,data= datas)
+print(r.cookies)
+#手动获取到cookies返回的JSESSIONID
+coo = r.cookies["JSESSIONID"]
+#通过字典的形式引用cookies返回的JSESSIONID，放入下次请求的cookies中
+cooki = {
+    'cookies' : coo
+}
+url1 = "https://www.wanandroid.com/lg/todo/list/0"
+r1 = requests.get(url= url1,cookies = cooki)
+print(r1.status_code)
+"""
 ###########方法四##############通过定制cookie，放入header来访问目标网址
-
-
-
-
+"""
+# 1.请求地址
+urlstr = "https://wanandroid.com/user/login"
+# 2.发送请求
+datas = {"username":"yanjie000","password":"123123"}
+r = requests.post(url = urlstr,data= datas)
+#手动获取cookies返回的JSESSIONID
+coo = r.cookies["JSESSIONID"]
+#通过字典的形式引用cookies返回的JSESSIONID，放入下次请求的headers中
+herder = {
+    "cookie":"JSESSIONID="+coo   #通过拼接形式将JSESSIONID = A2751057CAC77A085A4FF25A55EE7D2C拼接起来
+}
+url1 = "https://www.wanandroid.com/lg/todo/list/0"
+r1 = requests.get(url= url1,headers = herder)
+print(r1.status_code)
+"""
 
 
 
