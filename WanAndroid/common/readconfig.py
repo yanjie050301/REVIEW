573da05d81a465164e5fcff8557153e60c50cb75
yanjie050301 <1353037583@qq.com>
2021-04-08 14:54:57 +0800

    上传练习代码（12，,13）

--- WanAndroid/common/readconfig.py
+++ WanAndroid/common/readconfig.py
@@ -1,13 +1,40 @@
 import configparser
 import os
+"""
+1、查找config.ini的路径
+2、传值option和section获取相关信息的信息
+
+"""
+#获取config文件夹上级的地址
+# path = os.path.dirname(os.path.dirname(__file__))
+#拼接config.ini文件地址
+# new_path = path +"\\config\\config.ini"
+#实例化configparser
+# conf=configparser.ConfigParser()
+#读取config.ini文件
+# conf.read(new_path,encoding="utf-8")
+#获取所以的sections
+# sections = conf.sections()
+#获取某个section下的所有options
+# options = conf.options("host")
+#获取某个section下的多有options的键值对
+# value = conf.items("host")
+#获取某个section的options的值
+# gets = conf.get("host","uu")
+# print(gets)
+
+class Readconfig():
+    def __init__(self,sec):
+        self.sec = sec
+        path =os.path.dirname(os.path.dirname(__file__)) #获取config文件夹上级的地址
+        self.new_path = path + "\\config\\config.ini"   #拼接config.ini文件地址
+        self.conf = configparser.ConfigParser()
+    def readcon(self,opt):
+        self.conf.read(self.new_path,encoding="utf-8")
+        data = self.conf.get(self.sec,opt)    #获取具体的section下的某个option的值
+        return data
 
 
-path = os.path.dirname(os.path.dirname(__file__))
-new_path = path +"\\config\\config.ini"
-conf=configparser.ConfigParser()
-conf.read(new_path,encoding="utf-8")
-sections = conf.sections()
-options = conf.options("host")
-value = conf.items("host")
-gets = conf.get("host","uu")
-print(gets)
+if __name__ == '__main__':
+    a = Readconfig("host")
+    a.readcon("uu")
