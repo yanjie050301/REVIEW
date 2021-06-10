cb9f40a0f7bc7138731d29b3b544d0ebbf09c5d0
yanjie050301 <1353037583@qq.com>
2021-06-10 16:52:14 +0800

    上传appium启动APP代码

--- UiAutoTest_chao/appTest.py
+++ UiAutoTest_chao/appTest.py
@@ -7,11 +7,20 @@ desired_caps = {
   "platformName": "Android",
   "platformVersion": "9",
   "deviceName": "vivo X23",
-  "appPackage": "com.lchr.diaoyu",
-  "appActivity": "com.lchr.diaoyu.SplashActivity",
-  "app": "F:\\test\\selenium\\diaoyu_3.4.11_wap_release.apk",
+  "appPackage": "com.ss.android.article.news",
+  "appActivity": "com.ss.android.article.news.activity.MainActivity",
+  "app": "F:\\test\\selenium\\jinritoutiao.apk",
   "skipServerInstallation": True,
   "noReset": True
 }
-driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-time.sleep(10)
+driver = webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
+print("启动成功")
+time.sleep(5)
+#点击发布按钮
+driver.find_element_by_id("com.ss.android.article.news:id/d10").click()
+time.sleep(2)
+#点击微头条
+driver.find_element_by_id("com.ss.android.article.news:id/eor").click()
+time.sleep(5)
+
+driver.quit()
