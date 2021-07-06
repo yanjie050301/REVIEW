738cbe391c07943c49ca1cc10591fbae1c534da6
yanjie050301 <1353037583@qq.com>
2021-07-06 10:06:45 +0800

    上传练习代码（43）

--- python_lianxi/lianxi41-50.py
+++ python_lianxi/lianxi41-50.py
@@ -73,8 +73,21 @@ with open(name+".txt","w",encoding="utf-8") as f:
             break
         f.write(s)
 """
+"""
+****************四十三、从键盘输入一个字符串，将小写字母全部转换成大写字母，然后输出到一个磁盘文件"test"中保存。
+思想：
+"""
 
+def sinput():
+    s = input("请输入一个字符串：")
+    a = s.upper()
+    print(f"转化为大写字母后为：{a}")
+    with open("test.txt","w",encoding="utf-8") as f:
+        f.write(a)
+    print("写入磁盘成功！")
 
+if __name__ == '__main__':
+   sinput()
 
 
 
